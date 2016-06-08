6656c5def38402244ea5378d9f024b84c4c99c63
Spripri15 <opaline63live.fr@gmail.com>
2016-06-08 15:56:41 +0200

    Create Reference.java

---
+++ src/main/java/net/mod/driversanfranciscraft/Reference.java
@@ -0,0 +1,13 @@
+package net.mod.driversanfranciscraft;
+
+public class Reference
+{
+
+	public static final String MOD_ID = "driversanfranciscraft";
+	public static final String MOD_NAME = "Driver San FrancisCraft";
+	public static final String VERSION = "1.0.0";
+	public static final String LLIBRARY_VERSION = "1.3.0";
+	public static final String CLIENT_PROXY = "net.mod.driversanfranciscraft.proxy.ClientProxy";
+	public static final String SERVER_PROXY = "net.mod.driversanfranciscraft.proxy.CommonProxy";
+
+}
