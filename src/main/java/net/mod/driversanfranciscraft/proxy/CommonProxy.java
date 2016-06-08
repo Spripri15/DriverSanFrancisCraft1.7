5c731b2655f9dfc0ab8e2d5fbff41dd5332d7e3e
Spripri15 <opaline63live.fr@gmail.com>
2016-06-08 15:58:37 +0200

    Create CommonProxy.java

---
+++ src/main/java/net/mod/driversanfranciscraft/proxy/CommonProxy.java
@@ -0,0 +1,13 @@
+package net.mod.driversanfranciscraft.proxy;
+
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+
+public class CommonProxy
+{
+	public void registerRenderers()
+	{
+		
+	}
+}
+
+
