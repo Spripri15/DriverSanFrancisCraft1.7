7ebfd5a11d5810e623e5b8fc2434e46bbaebce1c
Spripri15 <opaline63live.fr@gmail.com>
2016-06-08 15:58:13 +0200

    Create ClientProxy.java

---
+++ src/main/java/net/mod/driversanfranciscraft/proxy/ClientProxy.java
@@ -0,0 +1,14 @@
+package net.mod.driversanfranciscraft.proxy;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class ClientProxy extends CommonProxy 
+{
+	@Override
+	public void registerRenderers(){
+		
+	}
+	
+}
